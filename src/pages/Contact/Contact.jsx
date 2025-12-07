d6a214b1dc7cab87e46e5bca2ecf375d06adf0c9
chowdhury-yeamin <chowdhuryyeamin07@gmail.com>
2025-12-07 22:03:06 +0600

    style: apply color scheme to contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -23,6 +23,7 @@ const Contact = () => {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      {/* Contact page with color scheme */}
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
