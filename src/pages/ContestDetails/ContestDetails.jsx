cd76baba63f9d24af14654b07e2e79580d0fcc69
chowdhury-yeamin <chowdhuryyeamin07@gmail.com>
2025-12-07 22:02:50 +0600

    style: apply color scheme to contest details page

--- src/pages/ContestDetails/ContestDetails.jsx
+++ src/pages/ContestDetails/ContestDetails.jsx
@@ -149,6 +149,7 @@ const ContestDetails = () => {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6 md:py-8">
+      {/* Contest details with color scheme */}
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
