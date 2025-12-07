a082ebc0821c25a26d23f4f382ab8f32ee410559
chowdhury-yeamin <chowdhuryyeamin07@gmail.com>
2025-12-07 21:58:45 +0600

    style: update home page with new color palette

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      {/* Banner Section */}
+      {/* Banner Section with primary color */}
       <motion.section
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
